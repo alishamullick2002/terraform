83d86de68f6e358ecd8855bd4e58ead95b7bf8e9
alisha <alishamullick2002@gmail.com>
2026-06-03 11:40:17 +0530

    day-4

--- Day-3-statefiles/main.tf
+++ Day-3-statefiles/main.tf
@@ -1,5 +1,9 @@
 resource "aws_instance" "name" {
     ami = "ami-00e801948462f718a"
     instance_type = "t2.medium"
-
+    
+ tags = {
+   name = "Alisha"
+   
+ }
 }
---
+++ Day-4-remote-backup/main.tf
@@ -0,0 +1,27 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "alisha-vpc"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "alisha-subnet1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "alisha-subnet2"
+  }
+}
