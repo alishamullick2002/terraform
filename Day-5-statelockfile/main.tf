9817102c8175fa0b9cf084bbe0348cc98e465fae
root <root@ip-172-31-0-195.ec2.internal>
2026-06-04 06:05:23 +0000

    day-5 updated in linux

--- Day-5-statelockfile/main.tf
+++ Day-5-statelockfile/main.tf
@@ -15,3 +15,13 @@ resource "aws_subnet" "subnet1" {
     Name = "alisha-subnet1"
   }
 }
+
+resource "aws_subnet" "subnet3" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "alisha-subnet3"
+  }
+}
